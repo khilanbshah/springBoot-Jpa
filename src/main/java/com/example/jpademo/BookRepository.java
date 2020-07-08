9aad530f09d6abf7dad202a2c6f36612f9f38937
khilan <khilan.shah@gmail.com>
2020-07-08 16:18:11 +0530

    jpa persistance demo from https://mkyong.com/spring-boot/spring-boot-spring-data-jpa/

---
+++ src/main/java/com/example/jpademo/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.jpademo;
+
+import com.example.jpademo.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findByName(String name);
+}
